0de607e20d12e9440729e02c722028b59e3d935a
sh_rn <shrnkld@pm.me>
2022-09-15 12:46:00 +0200

    update login signing msg content

--- src/configs/index.jsx
+++ src/configs/index.jsx
@@ -1,1 +1,1 @@
-export const NEW_PROJECT_MSG = 'create project'
+export const NEW_PROJECT_MSG = 'XQuery uses a cryptographic signature to verify that you are the owner of this Ethereum address. Please sign this tx to login.'
