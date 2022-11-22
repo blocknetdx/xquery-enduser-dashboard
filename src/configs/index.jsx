20386b06ac217c20498c03efb2cdf620af55851d
sh_rn <shrnkld@pm.me>
2022-11-22 13:21:53 +0100

    Add new dev staging server

--- src/configs/index.jsx
+++ src/configs/index.jsx
@@ -1,2 +1,2 @@
 export const NEW_PROJECT_MSG = 'XQuery uses a cryptographic signature to verify that you are the owner of this Ethereum address. Please sign this tx to login.'
-export const ENDPOINT = 'http://143.198.144.71:5432/api'
+export const ENDPOINT = 'http://165.22.21.220:5432/api'
