3c0cb421b59d9ad47b1d7412ed273f5d99b84b81
Shorn <shrnkld@pm.me>
2023-02-27 10:37:58 +0100

    group project table filter items by status
    - group relevant project statuses
    - add new menu labels for groups
    - fix filter pill css width

--- src/configs/index.jsx
+++ src/configs/index.jsx
@@ -1,2 +1,3 @@
 export const NEW_PROJECT_MSG = 'XQuery uses a cryptographic signature to verify that you are the owner of this Ethereum address. Please sign this tx to login.'
-export const ENDPOINT = 'http://localhost:5433/api'
+export const ENDPOINT = 'https://app.xquery.io/api' // app remote site
+//export const ENDPOINT = 'http://localhost:5433/api' // local testing only
